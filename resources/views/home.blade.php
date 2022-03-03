0ab1b984c5ce5f5f64a72a36cf07847188763033
Dipesh Rawat <deepeshrawat77@gmail.com>
2022-03-03 21:28:33 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
             <div class="card-header">{{ __('Brief Description') }}</div>
             <div class="card-body">
                 <p class="card-text">
-                PAMEL is a data fusion architecture that supports the development of MMLA systems that can process multimodal evidence of learning - collected and stored in heterogeneous data sources by taking contextual information of the learning situation into account. This infrastructure can process multimodal data collected from a set of learning tools/platforms - . Prior to the processing of the multimodal evidence, it also allows multi-stakeholders to enter the contextual information of the learning situation!!
+                Processing Architecture for Multimodal Experience of Learning (PAMEL) is an MMLA architecture that supports the development of MMLA solutions. PAMEL is designed by following Separation-of-Concerns (SoC) architectural design principle. It supports modularity and modifiability features that help developers to develop such MMLA solutions that are context-aware in nature. It follows a standard data processing pipeline that deals with each of the concerns involved in processing multimodal educational data by considering contextual information. It has different modules that help in achieving the objective of fusing multimodal educational data to generate multimodal experience of learning. through different modules. Finally, each of the modules are modifiable that help developers to reuse the developed modules in one solution with minor modifications in another solution.
                 </p>
             </div>
             </div>
